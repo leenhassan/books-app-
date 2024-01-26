7c64a163e795321c4e67c9e07e7e22097c701b3c
leen <leenhassan31@gmail.com>
2024-01-26 10:38:02 +0100

    commit git

---
+++ main.py
@@ -0,0 +1,39 @@
+# app.py
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+# Define Book model
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+
+# Route to display list of books
+@app.route('/books')
+def books():
+    all_books = Book.query.all()
+    return render_template('books.html', books=all_books)
+
+# Route to add a new book
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        publication_year = request.form['publication_year']
+        new_book = Book(title=title, author=author, publication_year=publication_year)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('books'))
+    return render_template('add_book.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
